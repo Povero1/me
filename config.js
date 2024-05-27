64ebb91166cbb85147edfd9363ae13d0de7458ab
Povero1 <163955809+Povero1@users.noreply.github.com>
2024-05-28 02:46:11 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bot: {
+    owners: ["889994885212553257"],  // اونر
+    botID: "1244780745009135737",    // ايدي البوت
+    GuildId: "1241783757644759061",   // ايدي السيرفير
+    ClientId: "1244780745009135737",    // ايدي البوت
+    serverinvte: "https://discord.gg/KwB2kWqGfP", // انفايت سير
+    clientSECRET: process.env.client , // سكريت
+    callbackURL: "https://d6cc7ba8-dcad-4916-a105-f56206d28006-00-2qwuglubjfzi5.kirk.replit.dev//login", // الكال باك
+    inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1244780745009135737&permissions=1&scope=bot",// انفايت البوت
+    TheLinkVerfy : 'https://discord.com/oauth2/authorize?client_id=1244780745009135737&response_type=code&redirect_uri=https%3A%2F%2Fd6cc7ba8-dcad-4916-a105-f56206d28006-00-2qwuglubjfzi5.kirk.replit.dev%2F%2Flogin&scope=identify+guilds.join', // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix : '$', 
+    ceatogry : '1244775545389191330', // كاتوجري الي يفتح فيها تكت شراء
+     TOKEN: (process.env.midd),// توكن 
+    Price: 1,    // سعر العضو الواحد
+    TraId  : '889994885212553257' // الي يتحوله كريديت
+  },
+  website: {
+    PORT: "3001",
+  }
+}
